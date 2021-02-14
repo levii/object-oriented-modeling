eee5e31f180fa8f22dff6a112979c7fe1c770d6e
Akira Yumiyama <yumiyama.akira@gmail.com>
2021-02-14 18:14:41 +0900

    chapter2 ポーカーの例を実装

--- poker/index.ts
+++ poker/index.ts
@@ -73,3 +73,56 @@ class Suit {
 }
 
 export { Hand, Card, Rank, Suit };
+
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+interface IPokerHand {}
+
+class OnePairPokerHand implements IPokerHand {
+    readonly pair: [Card, Card];
+
+    constructor(cardA: Card, cardB: Card) {
+        if (cardA.rank === cardB.rank) {
+            this.pair = [cardA, cardB];
+        } else {
+            throw new Error(`Invalid cards: ${cardA}, ${cardB}`);
+        }
+    }
+}
+
+class ThreeCardPokerHand implements IPokerHand {
+    readonly cards: [Card, Card, Card];
+
+    constructor(cardA: Card, cardB: Card, cardC: Card) {
+        if (cardA.rank === cardB.rank && cardB.rank === cardC.rank) {
+            this.cards = [cardA, cardB, cardC];
+        } else {
+            throw new Error(`Invalid cards: ${cardA}, ${cardB}, ${cardC}`);
+        }
+    }
+}
+
+class FullHousePokerHand implements IPokerHand {
+    readonly pair: [Card, Card];
+    readonly threeCards: [Card, Card, Card];
+
+    constructor(pair: [Card, Card], threeCards: [Card, Card, Card]) {
+        this.pair = pair;
+        this.threeCards = threeCards;
+
+        if (this.pair[0].rank !== this.pair[1].rank) {
+            throw new Error(`Invalid pair: ${this.pair}`);
+        }
+        if (
+            this.threeCards[0].rank !== this.threeCards[1].rank ||
+            this.threeCards[0].rank !== this.threeCards[2].rank
+        ) {
+            throw new Error(`Invalid cards: ${this.threeCards}`);
+        }
+
+        if (this.pair[0].rank === this.threeCards[0].rank) {
+            throw new Error(`Invalid cards`);
+        }
+    }
+}
+
+export { IPokerHand, OnePairPokerHand, ThreeCardPokerHand, FullHousePokerHand };
