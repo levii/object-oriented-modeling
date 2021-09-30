ad1adbdcc5d79bd174a27b9b4b20fba249583d86
Akira Yumiyama <yumiyama.akira@gmail.com>
2021-09-30 11:00:37 +0900

    Compare関数を追加

--- poker/chapter4.snapshot.ts
+++ poker/chapter4.snapshot.ts
@@ -2,6 +2,11 @@ import { Rank, Suit } from './index';
 
 class Card {
     constructor(private readonly suit: Suit, private readonly rank: Rank) {}
+
+    compareTo(other: Card): number {
+        // TODO: this と other のカードの強さを比較する。（まずランクで強弱比較して、それが同じ場合にはスートで比較する)
+        return 0;
+    }
 }
 
 class Hand {
@@ -33,7 +38,18 @@ class OnePair implements IPokerHand {
     }
 
     compareByStrength(other: IPokerHand): number {
-        return 0; // TODO
+        if (other instanceof OnePair) {
+            return this.compareWithOnePair(other);
+        } else {
+            return 0; // TODO
+        }
+    }
+
+    private compareWithOnePair(other: OnePair): number {
+        // TODO: this.cards と other.cards で強さを比較する。
+        //  具体的な比較は、 card.compareTo(card) や rank.compareTo(rank) に移譲する。
+        //  ワンペアの場合には、ペア同士のランクで比較して、それが同じ場合にはスートで強さを決める。
+        return 0;
     }
 }
 
@@ -48,6 +64,17 @@ class TwoPair implements IPokerHand {
     }
 
     compareByStrength(other: IPokerHand): number {
-        return 0; // TODO
+        if (other instanceof TwoPair) {
+            return this.compareWithTwoPair(other);
+        } else {
+            return 0; // TODO
+        }
+    }
+
+    private compareWithTwoPair(other: TwoPair): number {
+        // TODO: this と other で強さを比較する。
+        //  ツーペアの場合には、強い方のペア同士のランクで比較して、それが同じ場合には弱い方のペア同士で比較する流れになる。
+        //  NOTE: 弱い方のペア同士のランクが同じ場合にどちらが強いと判断するかは要確認。
+        return 0;
     }
 }
